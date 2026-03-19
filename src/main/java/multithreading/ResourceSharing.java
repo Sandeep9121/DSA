4771bcd0db4b1ef2bd4aa17c2c6aff408bcc811e
Sandeep Kumar Rayala <sandeepkumarrayala@Sandeeps-MacBook-Air.local>
2026-03-20 00:29:33 +0530

    Simple 2 threads using the shared resource

---
+++ src/main/java/multithreading/ResourceSharing.java
@@ -0,0 +1,34 @@
+package multithreading;
+
+public class ResourceSharing {
+
+    public  void useResource(String threadName){
+        System.out.println(threadName +" using the resource");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+         ResourceSharing rs=new ResourceSharing();
+
+        Runnable task = ()->{
+            for (int i = 0; i < 3; i++) {
+                rs.useResource(Thread.currentThread().getName());
+            }
+        };
+
+        Thread t1=new Thread(task,"t1");
+        Thread t2=new Thread(task,"t2");
+
+        t1.start();
+        t2.start();
+
+
+
+    }
+}
--- src/main/java/zClasses/basicloops/Test.java
+++ src/main/java/zClasses/basicloops/Test.java
@@ -2,7 +2,55 @@ package zClasses.basicloops;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) return 1;   // base case
+        return n * factorial(n - 1);      // recursive call
+    }
+
+
+    public static void fibanocii(int size){
+        int a=0; int b=1;
+        int i=0;
+       while(i<size){
+           System.out.print(" "+a);
+           int temp=a;
+           a=a+b;
+           b=temp;
+           i++;
+       }
 
     }
+
+    public  static int fibanocciRec(int n){
+        if(n<=1) return n;
+        return  fibanocciRec(n-1) + fibanocciRec(n-2);
+    }
+
+    public static int sum(int n) {
+        if (n == 0) return 0;
+        return n + sum(n - 1);
+    }
+
+    public static String reverse(String s){
+        if(s.isEmpty()) return s;
+        return reverse(s.substring(1))+s.charAt(0);
+    }
+
+
+    public static void main(String[] args) {
+        //System.out.println(factorial(10));
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print(" "+fibanocciRec(i));
+        }
+        System.out.println();
+
+        System.out.println(sum(10));
+
+
+    }
+
+
+
+
 }
